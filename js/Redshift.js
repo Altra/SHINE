4e8676af4ea77d361904c888d2556d868f9d0be8
Altra <flyhiy1@gmail.com>
2016-01-10 23:33:19 +0000

    First commit

---
+++ js/Redshift.js
@@ -0,0 +1,105 @@
+//
+//  Redshift.js
+
+function updateSpectra() {
+	z = getSlider(sliderSpec,300);
+	if (z == lastSliderSpec){
+	 requestAnimationFrame(updateSpectra);
+	 return;
+    }
+	lastSliderSpec = z;
+	var l = 335;
+	var y = 50;
+
+	ctxS.fillStyle='black';
+	ctxS.fillRect(0,0,335,40);
+
+	//Original
+	ctxS.fillStyle=drawSpectra(l, ctxS);
+	ctxS.fillRect(0,0,335,40);
+	drawAbsorption(0, 0, ctxS);
+
+	//Redshifted
+	ctxS.fillStyle=drawSpectra(l, ctxS);
+	ctxS.fillRect(0,50,335,40);
+	drawAbsorption(z*2, y, ctxS);
+
+	v = (z*(3e8))/1000;//Divided by 1000 to convert m/s to km/s
+	d = (v/67.80)*3.26156; //Multiplied by the constant to cover megaparsecs to lightyears
+	vt =  v.toExponential(2);
+	dt = d.toExponential(2);
+	if (vt == "0.00e+0"){
+  	 tx.innerHTML = "The velocity of the galaxy is 0 km/s";
+	 com.innerHTML = 'The galaxy is not moving relative to us so its absorption spectrum has not changed.';
+	 com2.innerHTML = '';
+	} else {
+ 	 vt = vt.replace('e+','x10^');
+	 tx.innerHTML = "The velocity of the galaxy is " + vt + " km/s";
+	 if (v > 0) {
+	  dt = dt.replace('e+','x10^');
+	  com.innerHTML = 'The galaxy is moving away from us so its absorption spectrum is redshifted.';
+	  com2.innerHTML = ('Nearly all galaxies are moving away from us due to the universe expanding. We can work out the distance to the galaxy because the further away it is the faster it will be moving. This one would be ~' + dt + ' lightyears away.');
+	 } else {
+	  com.innerHTML = 'The galaxy is moving towards us so its absorption spectrum is blueshifted.';
+	  com2.innerHTML = 'There are few galaxies moving towards us. The most well known is Andromeda, which will eventually collide with our own galaxy, the Milky Way.';
+	 }
+	}
+
+	 requestAnimationFrame(updateSpectra);
+}
+	
+function drawSpectra(l, ctxS) {
+  var grd=ctxS.createLinearGradient(0,0,l,0);
+	grd.addColorStop(0,'rgb(0,0,0)');
+	grd.addColorStop(0.08,'rgb(73,0,140)');
+	grd.addColorStop(0.20,'rgb(38,4,235)');
+	grd.addColorStop(0.30,'rgb(1,148,227)');
+	grd.addColorStop(0.36,'rgb(0,216,136)');
+	grd.addColorStop(0.5,'rgb(1,183,0)');
+	grd.addColorStop(0.68,'rgb(221,204,0)');
+	grd.addColorStop(0.77,'rgb(255,95,0)');
+	grd.addColorStop(0.84,'rgb(235,38,0)');
+	grd.addColorStop(0.9,'rgb(200,12,0)');
+	grd.addColorStop(1,'rgb(0,0,0)');
+
+	return grd;
+}
+
+ function drawAbsorption(x, y, ctxS){
+	ctxS.fillStyle='black';
+	ctxS.fillRect(32 + x,y,2,40);
+	ctxS.fillRect(61 + x,y,2,40);
+	ctxS.fillRect(125 + x,y,3,40);
+	ctxS.fillRect(305 + x,y,3,40);
+}
+
+function getSlider(sliderSpec,width){
+ perS = (parseInt(sliderSpec.style.left)-230)/width;
+ return 12*(perS - 0.5);
+}
+
+function startSpec(){
+ cS = document.getElementById("spectra");
+ ctxS = cS.getContext("2d");
+ tx = document.getElementById('dis');
+ com = document.getElementById('com');
+ com2 = document.getElementById('com2');
+ com3 = document.getElementById('com3');
+ 
+ new Slider(document.getElementById('sliderSpec'), 230, 530, "Images/Slider");
+ sliderSpec = document.getElementById('sliderSpec');
+ lastSliderSpec = 999;
+ 
+  requestAnimationFrame = window.requestAnimationFrame || 
+                            window.mozRequestAnimationFrame || 
+                            window.webkitRequestAnimationFrame || 
+                            window.msRequestAnimationFrame;
+ 
+ updateSpectra();
+}
+
+window.onload = function(){
+  Maths = Math;
+ $(document).ready(function() {Tipped.create('.inline');});
+ startSpec();
+}
---
+++ js/sound.js
@@ -0,0 +1,181 @@
+
+function drawGraph(){
+
+ctxD.strokeStyle =  "#00f";
+ctxD.lineWidth = 4;
+
+for(var z = 0; z < waves.length; z++){
+ waves[z].r += waveSpeed;
+ ctxD.beginPath();
+ ctxD.arc(200, 200, waves[z].r, 0, 2 * Math.PI);
+ ctxD.stroke();
+}
+}
+
+function addWave(){
+ waves.push({ox: 200, r: 10});
+}
+
+function removeWave(){
+ waves.shift();
+}
+
+function updateSound(){
+
+ freq = getFreq(slider,190);
+ useFreq = (1/freq)*200000;
+ ctxD.clearRect(0, 0, cD.width, cD.height);
+ if (waveSpawnTime > useFreq) {
+  addWave();
+  waveSpawnTime = 0;
+  } else {
+  waveSpawnTime++;
+ }
+ if(waves.length > 1){console.log(waves[0].r );}
+ if (waves.length > 1){
+   if(waves[0].r > 275) {
+  removeWave();
+ }}
+ 
+ drawGraph();
+ ctxD.drawImage(soyuz,150,170);
+ 
+  dcom.innerHTML =  freq.toExponential(2).replace('e+','x10^') + "Hz = ";
+  d2com.innerHTML =  (343/freq).toExponential(2).replace('e+','x10^').replace('e-','x10^-') + "m"; //Wavelength from div by speed of sound
+  if(freq>19900){
+  d3com.innerHTML = "This sound wave is the maximum frequency that humans can hear at 20,000Hz. We call sound waves with frequencies above this ultrasound. ";
+  } else if(freq==1000) { d3com.innerHTML = "Humans cannot hear frequencies below 20Hz but this 1000Hz would still sound deep (It has a low pitch)."; }
+ else { d3com.innerHTML = "The sound wave is drawn to a scale as it would be hard to see something that oscillates over 1000 times per second.";}
+ t++;
+ updateOs();
+ requestAnimationFrame(updateSound);
+}
+
+function updateOs(){
+   ctxW.clearRect(0, 0, cD.width, cD.height);  
+    
+ Vp=1; //Max Amplitue
+ fo=freq //frequancy in Hz
+ phase=-t;
+ Vmax=2;
+ Tmax=0.0005;
+ N=300;
+     
+  // define origin at plot center
+ var axes={};
+ axes.x0 = 0.5 + 0.5*cW.width;  // x0, y0 place plot origin in middle of cW
+ axes.y0 = 0.5 + 0.5*cW.height;
+ 
+ // draw axes
+ showAxes(ctxW,axes);
+ 
+ var x=new Array(), y=new Array();  // x,y plotting variables
+ var dt, tstart, tstop;             // time variables
+ 
+ // define plot paramaters
+ tstart=-Tmax;
+ tstop=Tmax;
+ dt = (tstop - tstart) / (N-1);				// time increment over N points
+ axes.xscale = (cW.width)/(2*Tmax); 	// x pix per s
+ axes.yscale = (cW.height)/(2*Vmax);    // y pix per V
+ axes.N = N;
+ 
+ 
+  // create function 
+ for ( i=0; i<N; i++) {
+ 	x[i]=tstart + i*dt;
+ 	y[i] = Vp*Math.sin(2*3.1415*fo*x[i] + phase*3.1415/180) ;
+ }
+ 
+ // display variables for debug, remove /* and */
+ /*
+ alert(   "N=" + N + "\n"
+        + "dt=" + dt + "\n"
+        + "tstart=" + tstart + "\n"
+        + "tstop=" + tstop + "\n"    
+        + "fo=" + fo + "\n"  
+        + "x[50]=" + x[50] + "\n"        );
+*/
+ 
+ // plot function
+ GraphArray(ctxW,axes,x,y,"rgb(0,0,256)",2); 
+ 
+ }
+ 
+function GraphArray (ctxW,axes,x,y,color,thick) {
+
+ var i, x0, y0, xscale, yscale, xp, yp;
+  
+ x0=axes.x0;  y0=axes.y0;
+ xscale=axes.xscale;  yscale=axes.yscale;
+
+ ctxW.beginPath();
+ ctxW.lineWidth = thick;
+ ctxW.strokeStyle = color;
+
+ for (i=0; i<axes.N; i++) {
+ 	// translate actual x,y to plot xp,yp
+ 	xp = x0 + x[i]*xscale;
+ 	yp = y0 - y[i]*yscale;
+ 	
+ 	// draw ine to next point
+	if (i==0) ctxW.moveTo( xp, yp );
+	else      ctxW.lineTo( xp, yp );
+ }
+ 
+ ctxW.stroke();
+}
+
+function showAxes(ctx,axes) {
+ var x0=axes.x0, w=cW.width;
+ var y0=axes.y0, h=cW.height;
+ 
+ ctxW.beginPath();
+ ctxW.strokeStyle = "rgb(128,128,128)"; 
+ ctxW.lineWidth = 1;
+ ctxW.moveTo(0,y0);    ctxW.lineTo(w,y0);  // X axis
+ ctxW.moveTo(x0,0);    ctxW.lineTo(x0,h);  // Y axis
+ ctxW.stroke();
+ }   
+
+function getFreq(slider,width){
+ per = (parseInt(slider.style.left)-200)/width;
+ per = per*20000;
+ if (per < 1) {
+ per = 1000;
+ }
+ return per;
+}
+
+function startSound(){
+ waves = [];
+ t = 0;
+ waveSpawnTime = 30;
+ waveRemoveDelayTime = 0;
+ removeWaves = false;
+ waveSpeed = 2;
+ soyuz = new Image();
+ soyuz.src = 'Images/Soyuz.png';
+
+ cD = document.getElementById("speaker");
+ ctxD = cD.getContext("2d");
+ cW = document.getElementById("wave");
+ctxW = cW.getContext("2d");
+ dcom = document.getElementById('dcom');
+ d2com = document.getElementById('d2com');
+ d3com = document.getElementById('d3com');
+
+ new Slider(document.getElementById('slider'), 200, 390, "Images/Slider");
+ slider = document.getElementById('slider');
+ 
+ updateSound();
+}
+window.onload = function(){
+  Maths = Math;
+ $(document).ready(function() {Tipped.create('.inline');});
+ requestAnimationFrame = window.requestAnimationFrame || 
+                            window.mozRequestAnimationFrame || 
+                            window.webkitRequestAnimationFrame || 
+                            window.msRequestAnimationFrame;
+ startSound();
+}
